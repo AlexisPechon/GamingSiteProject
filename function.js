22544f5eeeb7d41e70715ce62f13eb8b61ec2239
Alex P <56544034+AlexisPechon@users.noreply.github.com>
2023-04-03 20:36:34 +0100

    Second commit

--- function.js
+++ function.js
@@ -1,9 +1,19 @@
+// *Author: Alexis Pechon
+// *Student Number: x19358953
+// *Student Email: x19358953@student.ncirl.ie
+// *File: function.js
+// *Date: 28/02/2023
+
+//The purpose of the JavaScript file is to control the website's functionalities
+//this is mainly used in the login section of the webite
+
 //alert("Hello World!");
 
+//Creates a popup that asks the user for their input
 let userName = "";
 let popup = prompt("Please enter your name", userName);
 
-
+//If else statement that displays the name of the user they put in on the website itself
 if (popup != "") {
     document.getElementById("greetings").innerHTML =
     "Welcome back " + popup + "! How are you today?";
